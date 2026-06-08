69b02bd3b9648ab494b9a27d5abb0642ba8cb1d9
Mayank Joshi <MAYANKJOSHI.25101280629@gehu.ac.in>
2026-06-08 16:27:13 +0530

    Created the functions to be get performed over db with the help of ORM.

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -19,3 +19,20 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func CreateBook(b *Book) *Book {
+	db.Create(b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var book []Book
+	db.Find(&book)
+	return book
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	res := db.Where("id = ?", Id).First(&getBook)
+	return &getBook, res
+}
