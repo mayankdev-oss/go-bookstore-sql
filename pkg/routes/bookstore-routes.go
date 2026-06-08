61b6aefcb625243d0e4bada60e835b4cfe793896
Mayank Joshi <MAYANKJOSHI.25101280629@gehu.ac.in>
2026-06-08 16:49:22 +0530

    setuped routes for the url using gorilla mux and restricited Methods accn to the operation done by handler funcs

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mayankdev-oss/go-bookstore-sql/pkg/controllers"
+)
+
+var RegisterBookstoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.GetBookbyId).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookid}", controllers.DeleteBook).Methods("DELETE")
+}
